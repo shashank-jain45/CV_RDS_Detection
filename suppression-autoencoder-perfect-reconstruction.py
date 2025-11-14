85156b81d50bdf8ce4e7cc0dfd6d661ee3403db0
Shashank <shashank45jain@gmail.com>
2025-11-14 14:57:49 +0530

    "final-train-model"

--- suppression-autoencoder-perfect-reconstruction.py
+++ suppression-autoencoder-perfect-reconstruction.py
@@ -1,54 +1,70 @@
-#!/usr/bin/env python
-
-# # **Imports**
-
-
-import os 
+# Common
+import os
 import keras
-import numpy as np 
+import numpy as np
 from glob import glob
 import tensorflow as tf
 
-
+# Images
 import cv2 as cv
 from keras.preprocessing.image import img_to_array
 
+# Visualization
 import matplotlib.pyplot as plt
 
+# Model
 from keras import Sequential
 from keras.layers import Conv2D, BatchNormalization, MaxPool2D,Conv2DTranspose, Reshape, InputLayer
 
+model_save_path = './working/bone_suppression_autoencoder.keras'
+epoch_save_path = "./epochs/"
+# Extract directory part
+directory = os.path.dirname(model_save_path)
+directory2 = os.path.dirname(epoch_save_path)
 
-# # **Image Data**
+# Create directory if not present
+os.makedirs(directory, exist_ok=True)
+os.makedirs(directory2, exist_ok=True)  
+
+print("Directory ready:", directory)
+
+source_image_path = './archive/augmented/augmented/source/'
+target_image_path = './archive/augmented/augmented/target/'
+
+# Get Images
+source_image_names = sorted(glob(source_image_path + "*.png"))
+target_image_names = sorted(glob(target_image_path + "*.png"))
+
+SIZE = 256
+
+source_images = []
+target_images = []
+
+for img_path in source_image_names:
+    img = cv.imread(img_path)
+    img = cv.resize(img, (SIZE, SIZE), cv.INTER_CUBIC)
+    img = img_to_array(img)
+    img = img.astype('float')/255.
+    source_images.append(img)
+
+for img_path in target_image_names:
+    img = cv.imread(img_path)
+    img = cv.resize(img, (SIZE, SIZE), cv.INTER_CUBIC)
+    img = img_to_array(img)
+    img = img.astype('float')/255.
+    target_images.append(img)
 
 
-get_ipython().run_cell_magic('time', '', '\nsource_image_path = \'../input/xray-bone-shadow-supression/augmented/augmented/source/\'\ntarget_image_path = \'../input/xray-bone-shadow-supression/augmented/augmented/target/\'\n\n# Get Images\nsource_image_names = sorted(glob(source_image_path + "*.png"))\ntarget_image_names = sorted(glob(target_image_path + "*.png"))\n')
 
-
-
-
-source_image_names[:10]
-
-
-
-
-target_image_names[:10]
-
-
-
-get_ipython().run_cell_magic('time', '', "SIZE = 256\n\nsource_images = []\ntarget_images = []\n\nfor img_path in source_image_names:\n    img = cv.imread(img_path)\n    img = cv.resize(img, (SIZE, SIZE), cv.INTER_CUBIC)\n    img = img_to_array(img)\n    img = img.astype('float')/255.\n    source_images.append(img)\n\nfor img_path in target_image_names:\n    img = cv.imread(img_path)\n    img = cv.resize(img, (SIZE, SIZE), cv.INTER_CUBIC)\n    img = img_to_array(img)\n    img = img.astype('float')/255.\n    target_images.append(img)\n\nsource_images = np.array(source_images)\ntarget_images = np.array(target_images)\n")
-
-
-
+# Create directory if not present
+source_images = np.array(source_images)
+target_images = np.array(target_images)
 
 image_shape = source_images.shape[-3:]
-image_shape
 
-
-
-# # **Visualize**
-
-
+print("To check the number of images loaded: ")
+print(len(source_images))
+print(len(source_images))
 
 plt.figure(figsize=(10,10))
 for i in range(1,7):
@@ -62,20 +78,14 @@ for i in range(1,7):
         plt.title('Target Image')
     plt.axis('off')
 plt.tight_layout()
-plt.show()
-
-
-# # **AutoEncoder**
-
-
-
+plt.savefig(os.path.join(directory2,"image_grid.png"))
 
 class EncoderLayerBlock(keras.layers.Layer):
 
-    def __init__(self,filters):
+    def __init__(self, filters):
         super(EncoderLayerBlock, self).__init__()
         self.filters = filters
-        self.conv = Conv2D(filters,3,padding='same',activation='relu')
+        self.conv = Conv2D(filters, 3, padding='same', activation='relu')
         self.bn = BatchNormalization()
         self.pool = MaxPool2D()
 
@@ -89,43 +99,39 @@ class EncoderLayerBlock(keras.layers.Layer):
 
     def get_config(self):
         base_config = super().get_config()
-        return {**base_config, "Filters":self.filters}
-
+        return {**base_config, "Filters": self.filters}
 
 
 class ShowReconstruction(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         rand_id = np.random.randint(len(source_images))
-        source_image = source_images[rand_id][np.newaxis,...]
+        source_image = source_images[rand_id][np.newaxis, ...]
         reconstructed = self.model.predict(source_image)
         real_image = target_images[rand_id]
 
-        plt.subplot(1,3,1)
+        plt.subplot(1, 3, 1)
         plt.imshow(source_image[0])
         plt.title("Source Image")
         plt.axis('off')
 
-        plt.subplot(1,3,2)
+        plt.subplot(1, 3, 2)
         plt.imshow(reconstructed[0])
         plt.title("Produced Image")
         plt.axis('off')
 
-        plt.subplot(1,3,3)
+        plt.subplot(1, 3, 3)
         plt.imshow(real_image)
         plt.title("Real Image")
         plt.axis('off')
 
         plt.tight_layout()
-        plt.savefig("Reconstruction_Epoch_{}".format(epoch))
-        plt.show()
-
-
+        plt.savefig(os.path.join(directory2,"Reconstruction_Epoch_{}").format(epoch))
 
 
 autoencoder = Sequential([
 
     # Encoder
-    InputLayer(image_shape), # image_shape is now (256, 256, 3)
+    InputLayer(image_shape),  # image_shape is now (256, 256, 3)
     EncoderLayerBlock(32),
     EncoderLayerBlock(64),
     EncoderLayerBlock(128),
@@ -133,35 +139,28 @@ autoencoder = Sequential([
     # Latent Representation (Output is now 16x16x512)
     EncoderLayerBlock(512),
 
+    # Decoder
     Conv2DTranspose(128, kernel_size=3, strides=2, padding='same', activation='relu'),
 
-
+    # The rest remain 'same'
     Conv2DTranspose(64, kernel_size=3, strides=2, padding='same', activation='relu'),
     Conv2DTranspose(32, kernel_size=3, strides=2, padding='same', activation='relu'),
     Conv2DTranspose(3, kernel_size=3, strides=2, padding='same', activation='sigmoid'),
 
-    Reshape(image_shape) 
+    Reshape(image_shape)  # This will correctly reshape to (256, 256, 3)
 ])
-
-
-
-
 autoencoder.compile(
     loss='binary_crossentropy',
     optimizer= tf.keras.optimizers.Adam(learning_rate=0.001),
     metrics=['accuracy']
 )
 
-
-
 autoencoder.fit(
     source_images, target_images,
-    epochs=25,
+    epochs=30,
     callbacks=[ShowReconstruction()]
 )
-model_save_path = '/kaggle/working/bone_suppression_autoencoder.keras'
 
+# Save the entire model (architecture, weights, optimizer state, etc.)
 
 autoencoder.save(model_save_path)
-
-
